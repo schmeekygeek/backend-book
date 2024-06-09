b01a7105eb3cb4b36168ac2851899c7ed89360e6
schmeekygeek <ss6087982@gmail.com>
2024-06-09 17:37:58 +0530

    change project location

---
+++ wecart/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/schmeekygeek/wecart/api"
+)
+
+func main() {
+  http.HandleFunc("/", api.SayHello)
+
+  log.Println("Starting server on port :8080")
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Println("Error starting server:", err)
+  }
+}
