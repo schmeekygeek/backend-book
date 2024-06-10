9ccc5b419237e2cf5cd2f54b4d97f609fb8f8398
schmeekygeek <ss6087982@gmail.com>
2024-06-10 13:28:18 +0530

    add db connection init function

--- blogspace/data/personrepo.go
+++ blogspace/data/personrepo.go
@@ -1,14 +1,23 @@
-package db
+package data
+
+import (
+  "github.com/jmoiron/sqlx"
+  _ "github.com/lib/pq"
+)
+
+var DB *sqlx.DB
 
 type Person struct {
-  FirstName string
-  LastName  string
-  Email     string
-  Password  string
-  Liked     []BlogID
+  ID        int64  `db:"id" json:"id"` 
+  FirstName string `db:"first_name" json:"first_name"`
+  LastName  string `db:"last_name" json:"last_name"`
+  Username  string `json:"username"`
+  Email     string `json:"email"`
+  Password  string `json:"password"`
+  Liked     []BlogID `json:"liked"`
 }
 
-type BlogID string
+type BlogID int64
 
 type Blog struct {
   Title       string
@@ -16,6 +25,35 @@ type Blog struct {
   Content     string
 }
 
-func CreatePerson() {
+const (
+  CREATEPERSONTABLE = `
+    CREATE TABLE IF NOT EXISTS PERSON (
+      id serial unique,
+      first_name text,
+      last_name  text,
+      email      text,
+      password   text,
+      liked int8[]
+    );
+  `
 
+
+)
+
+func Init() error {
+  db, err := sqlx.Connect(
+    "postgres",
+    "user=postgres dbname=blogspace password=1213 sslmode=disable",
+  )
+  if err != nil {
+    return err
+  }
+  DB = db
+  DB.MustExec(CREATEPERSONTABLE)
+  return nil
+}
+
+func CreatePerson(p *Person) error {
+
+  return nil
 }
--- blogspace/main.go
+++ blogspace/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/schmeekygeek/wecart/api"
+	"github.com/schmeekygeek/blogspace/api"
+	"github.com/schmeekygeek/blogspace/data"
 )
 
 func main() {
   http.HandleFunc("/", api.SayHello)
+  http.HandleFunc("/person/create", api.CreatePerson)
 
   log.Println("Starting server on port :8080")
-  err := http.ListenAndServe(":8080", nil)
+  err := data.Init()
+  if err != nil {
+    panic(err)
+  }
+  err = http.ListenAndServe(":8080", nil)
   if err != nil {
     log.Println("Error starting server:", err)
   }
